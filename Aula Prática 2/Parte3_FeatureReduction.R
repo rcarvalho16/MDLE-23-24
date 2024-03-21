eeb798db11e64abc5bf98bd31db6524a4955ddda
RRamos19 <ricardo.alexandre.alves.ramos@gmail.com>
2024-03-21 17:45:04 +0000

    O b) e o c) do Feature Reduction estao praticamente feitos, falta so corrigir algumas cenas do dataset do pima

--- Aula Prática 2/Parte3_FeatureReduction.R
+++ Aula Prática 2/Parte3_FeatureReduction.R
@@ -30,18 +30,57 @@ corr_matrix_pima <- replace(corr_matrix_pima, is.na(corr_matrix_pima), 0)
 ggcorrplot(corr_matrix_lisbon)
 ggcorrplot(corr_matrix_pima)
 
+# a)
+
 # Apply PCA to the correlation matrixes
 data_lisbon_pca <- prcomp(corr_matrix_lisbon, scale. = FALSE)
 data_pima_pca <- prcomp(corr_matrix_pima, scale. = FALSE)
+summary(data_lisbon_pca)
+summary(data_pima_pca)
 
 # Plot PCA results
-summary(data_lisbon_pca)
 fviz_eig(data_lisbon_pca, addlabels = TRUE)
-
-summary(data_pima_pca)
 fviz_eig(data_pima_pca, addlabels = TRUE)
 
-#matrix_pca_lisbon <- matrix(unlist(data_lisbon_pca[5]), ncol = 18)
-#colnames(matrix_pca_lisbon) <- paste("PC",1:18, sep="")
-#rownames(matrix_pca_lisbon) <- colnames(data_lisbon)
-#View(matrix_pca_lisbon)
+# b)
+
+data_lisbon_svd <- svd(corr_matrix_lisbon)
+data_pima_svd <- svd(corr_matrix_pima)
+
+# Falta confirmar isto
+plot(1:length(data_lisbon_svd$d), data_lisbon_svd$d)
+plot(1:length(data_pima_svd$d), data_pima_svd$d)
+
+# c)
+
+# rd = reduced dimension
+rd_normalized_data_lisbon <- normalized_data_lisbon[, 1:4]
+#rd_normalized_data_pima <- normalized_data_pima[, ]
+
+# Computes the correlation matrixes
+rd_corr_matrix_lisbon <- cor(rd_normalized_data_lisbon)
+#rd_corr_matrix_pima <- cor(rd_normalized_data_pima)
+
+# Replace NAs with 0s using replace()
+rd_corr_matrix_lisbon <- replace(rd_corr_matrix_lisbon, is.na(rd_corr_matrix_lisbon), 0)
+#rd_corr_matrix_pima <- replace(rd_corr_matrix_pima, is.na(rd_corr_matrix_pima), 0)
+
+# Plot the correlation matrixes
+ggcorrplot(rd_corr_matrix_lisbon)
+
+# Apply PCA to the correlation matrixes
+rd_data_lisbon_pca <- prcomp(rd_corr_matrix_lisbon, scale. = FALSE)
+#rd_data_pima_pca <- prcomp(rd_corr_matrix_pima, scale. = FALSE)
+summary(rd_data_lisbon_pca)
+#summary(rd_data_pima_pca)
+
+# Plot PCA results
+fviz_eig(rd_data_lisbon_pca, addlabels = TRUE)
+#fviz_eig(rd_data_pima_pca, addlabels = TRUE)
+
+rd_data_lisbon_svd <- svd(corr_matrix_lisbon)
+#rd_data_pima_svd <- svd(corr_matrix_pima)
+
+# Falta confirmar isto
+plot(1:length(rd_data_lisbon_svd$d), rd_data_lisbon_svd$d)
+plot(1:length(rd_data_pima_svd$d), rd_data_pima_svd$d)
