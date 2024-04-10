8bc687c4dc8b87a49b23e58acda2735420b7802b
RRamos19 <ricardo.alexandre.alves.ramos@gmail.com>
2024-04-10 18:27:21 +0100

    A WeatherAnalysis foi toda feita apenas para o zipcode 1000. Foram adicionados dois metodos de FS nas helperFunctions

--- Trabalho_Pratico/Fase_1/WeatherAnalysis.R
+++ Trabalho_Pratico/Fase_1/WeatherAnalysis.R
@@ -64,144 +64,80 @@ energy_data$Date.Time <- apply(
 #)
 
 #######################################################
-# Compute the energetic consumption per icon in certain zip_codes in Lisbon City
+# Study how the weather affects the energy consumption for a certain zip code
+lisbon_zip_code <- 1000
 
-# Choose a list of 3 residential zip codes and 2 industrial zip codes
-# to study how the weather influences the energy consumption
-res_zip_codes <- c(1000, 1600, 1700)
-ind_zip_codes <- c(1300, 1250)
+# Obtain only the energy data related to the chosen zip code
+lisbon_zipcode_energy_data <- energy_data[energy_data$Zip.Code == lisbon_zip_code, ]
 
-energy_data_res <- energy_data[energy_data$Zip.Code %in% res_zip_codes, ]
-energy_data_ind <- energy_data[energy_data$Zip.Code %in% ind_zip_codes, ]
+# Merge the weather data with energy data
+lisbon_zipcode_consumption <- merge(weather_data, lisbon_zipcode_energy_data, by.x = "datetime", by.y = "Date.Time")
 
-# Join power consumption dataset to weather dataset by timestamp
-# Discard redundant columns (ie: Timestamp on both datasets is equal)
-lisbon_consumption_res <- merge(weather_data, energy_data_res, by.x = "datetime", by.y = "Date.Time")
-lisbon_consumption_ind <- merge(weather_data, energy_data_ind, by.x = "datetime", by.y = "Date.Time")
-# lisbon_consumption_weather <- merge(weather_data, energy_data, by.x = "datetime", by.y = "Date.Time")
-
-# Add day of the week
-lisbon_consumption_res$Day_of_Week <- wday(lisbon_consumption_res$Date, label = TRUE, abbr = FALSE)
-lisbon_consumption_ind$Day_of_Week <- wday(lisbon_consumption_ind$Date, label = TRUE, abbr = FALSE)
-# lisbon_consumption_weather$Day_of_Week <- wday(lisbon_consumption_weather$Date, label = TRUE, abbr = FALSE)
+# Add the day of week based on the date
+lisbon_zipcode_consumption$Day_of_Week <- wday(lisbon_zipcode_consumption$Date, label = TRUE, abbr = FALSE)
 
 # Remove redundant columns such as Zip.Code, name, datetime, stations, icon
 # Transform Date to Day, Month, Year, Hour
 # We can use conditions as a class label
 
 
-
-
-
-# Remove redundant Datetime, Lisbon tag
+# Remove redundant Datetime, Lisbon tag, Zip code, stations, icons
 remove <- c(1,2,23,24,27)
-# remove_lisbon <- c(1,2,23,24,27)
-lisbon_consumption_res <- lisbon_consumption_res[,-remove]
-lisbon_consumption_ind <- lisbon_consumption_ind[,-remove]
-# lisbon_consumption_weather <- lisbon_consumption_weather[,-remove_lisbon]
+lisbon_zipcode_consumption <- lisbon_zipcode_consumption[,-remove]
 
 # Transform Date to Day Month Year Hour (minutes are always 00 so no need)
 
-# CORRIGIR ISTO (N REMOVER NO LISBON_CONSUMPTION_RES O ZIP CODE)
-# Group by conditions and Zip.Code, calculating the mean of Energetic Consumption
-lisbon_consumption_res <- lisbon_consumption_res %>%
-  group_by(conditions, Day_of_Week, Zip.Code) %>%
-  summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
+# Normalize the energy consumption
+lisbon_zipcode_consumption$Active.Energy..kWh. <- lisbon_zipcode_consumption$Active.Energy..kWh./max(fixed_zipcode_lisbon_consumption$Active.Energy..kWh.)
 
-lisbon_consumption_ind <- lisbon_consumption_ind %>%
-  group_by(conditions, Day_of_Week, Zip.Code) %>%
-  summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
-
-industrial_plot <- list()
-residential_plot <- list()
-for(i in 1:length(ind_zip_codes)){
-  lisbon_consumption_ind_single_zip_code <<- lisbon_consumption_ind[lisbon_consumption_ind$Zip.Code %in% ind_zip_codes[i],]
-  
-  # Plot the average conditions consumption per Zip.Code bar chart
-  barra_ind <<- ggplot(lisbon_consumption_ind_single_zip_code, aes(x = conditions, y = Active.Energy..kWh., fill = factor(Day_of_Week))) +
-    geom_bar(stat = "identity", position = "dodge") +
-    labs(title = paste("Residential Consumption by conditions and for Zip Code", ind_zip_codes[i]),
-         x = "conditions",
-         y = "Average Consumption (kWh)",
-         fill = "Day of Week",
-         margin = element_text()) +
-    theme_minimal()
-  
-  industrial_plot[[i]] <- barra_ind
-  
-  rm(lisbon_consumption_ind_single_zip_code, barra_ind)
-}
-for(i in 1:length(res_zip_codes)){
-  lisbon_consumption_res_single_day <<- lisbon_consumption_res[lisbon_consumption_res$Zip.Code %in% res_zip_codes[i],]
-
-  barra_res <- ggplot(lisbon_consumption_res_single_day, aes(x = conditions, y = Active.Energy..kWh., fill = factor(Day_of_Week))) +
-    geom_bar(stat = "identity", position = "dodge") +
-    labs(title = paste("Industrial Consumption by conditions and for Zip Code", res_zip_codes[i]),
-         x = "conditions",
-         y = "Average Consumption (kWh)",
-         fill = "Day of Week",
-         margin = element_text()) +
-    theme_minimal()
-  
-  residential_plot[[i]] <- barra_res
-  
-  rm(lisbon_consumption_res_single_day, barra_res)
-}
-
-grid.arrange(grobs = industrial_plot, nrow = length(ind_zip_codes))
-grid.arrange(grobs = residential_plot, nrow = length(res_zip_codes))
+# Plot the average conditions consumption per Zip.Code bar chart
+ggplot(lisbon_zipcode_consumption, aes(x = conditions, y = Active.Energy..kWh., fill = factor(Day_of_Week))) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = paste("Residential Consumption by conditions and for Zip Code", lisbon_zip_code),
+       x = "conditions",
+       y = "Average Consumption (kWh)",
+       fill = "Day of Week",
+       margin = element_text()) +
+  theme_minimal()
 
 ########################################################
 # Check the influence of weather on the energy consumption
+  
+# Obtain the energy data of lisbon
+lisbon_zipcode_energy_data <- energy_data[energy_data$Zip.Code %in% lisbon_zip_code, ]
 
-zip_codes <- sample(energy_data$Zip.Code, 6)
+lisbon_zipcode_consumption <- merge(weather_data, lisbon_zipcode_energy_data, by.x = "datetime", by.y = "Date.Time")
+  
+# undersample the consumptions when its raining
+rain_conditions_lisbon_consumption <- lisbon_zipcode_consumption[lisbon_zipcode_consumption$conditions %in% c("Rain, Overcast", "Rain, Partially cloudy"), ]
+rain_conditions_lisbon_consumption$conditions <- "Rain"
 
-multiple_zip_code_plots <- list()
-for(i in 1:length(zip_codes)){
-  zip_code <- zip_codes[i]
+# undersample the consumptions when the weather is clear and the hours match with the hours when its raining
+clear_conditions_lisbon_consumption <- lisbon_zipcode_consumption[lisbon_zipcode_consumption$conditions %in% c("Clear") & lisbon_zipcode_consumption$Hour %in% rain_conditions_lisbon_consumption$Hour, ]
   
-  # Obtain the energy data of a certain zip_code
-  energy_data_res_ind <- energy_data[energy_data$Zip.Code %in% zip_code, ]
+# Remove ZIP Code and Redundant Lisbon tag
+#remove <- c(2, ncol(rain_conditions_lisbon_consumption) - 1)
+#rain_conditions_lisbon_consumption <- rain_conditions_lisbon_consumption[,-remove]
+#remove <- c(2, ncol(clear_conditions_lisbon_consumption) - 1)
+#clear_conditions_lisbon_consumption <- clear_conditions_lisbon_consumption[,-remove]
   
-  lisbon_zip_code_consumption <- merge(weather_data, energy_data_res_ind, by.x = "datetime", by.y = "Date.Time")
-  
-  # undersample the consumptions when its raining
-  rain_conditions_lisbon_consumption <- lisbon_zip_code_consumption[lisbon_zip_code_consumption$conditions %in% c("Rain, Overcast", "Rain, Partially cloudy"), ]
-  rain_conditions_lisbon_consumption$conditions <- "Rain"
-  
-  # undersample the consumptions when the weather is clear and the hours match with the hours when its raining
-  clear_conditions_lisbon_consumption <- lisbon_zip_code_consumption[lisbon_zip_code_consumption$conditions %in% c("Clear") & lisbon_zip_code_consumption$Hour %in% rain_conditions_lisbon_consumption$Hour, ]
-  
-  # Remove ZIP Code and Redundant Lisbon tag
-  remove <- c(2, ncol(rain_conditions_lisbon_consumption) - 1)
-  rain_conditions_lisbon_consumption <- rain_conditions_lisbon_consumption[,-remove]
-  remove <- c(2, ncol(clear_conditions_lisbon_consumption) - 1)
-  clear_conditions_lisbon_consumption <- clear_conditions_lisbon_consumption[,-remove]
-  
-  # Compute the mean consumption per hour
-  rain_conditions_lisbon_consumption <- rain_conditions_lisbon_consumption %>%
-    group_by(Hour, conditions) %>%
-    summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
-  
-  clear_conditions_lisbon_consumption <- clear_conditions_lisbon_consumption %>%
-    group_by(Hour, conditions) %>%
-    summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
-  
-  # Merge the raining table with the clear weather table
-  extreme_conditions_lisbon_consumption <- merge(rain_conditions_lisbon_consumption, clear_conditions_lisbon_consumption, all = TRUE)
-  
-  # Plot the line chart
-  zip_code_plot <- ggplot(extreme_conditions_lisbon_consumption, aes(x = Hour, y = Active.Energy..kWh., color = factor(conditions))) +
-    geom_line(aes(group = conditions)) +
-    labs(title = paste("Average Consumption by Hour and Weather Condition of zip code", zip_code),
-         x = "Hour of the Day",
-         y = "Average Consumption (kWh)",
-         color = "Weather Conditions") +
-    theme_minimal()
-  
-  # Append the plot to the list
-  multiple_zip_code_plots[[i]] <- zip_code_plot
-}
+# Compute the mean consumption per hour
+rain_conditions_lisbon_consumption <- rain_conditions_lisbon_consumption %>%
+  group_by(Hour, conditions) %>%
+  summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
 
-grid.arrange(grobs = multiple_zip_code_plots, nrow = 3)
+clear_conditions_lisbon_consumption <- clear_conditions_lisbon_consumption %>%
+  group_by(Hour, conditions) %>%
+  summarize(Active.Energy..kWh. = mean(Active.Energy..kWh.))
+  
+# Merge the raining table with the clear weather table
+extreme_conditions_lisbon_consumption <- merge(rain_conditions_lisbon_consumption, clear_conditions_lisbon_consumption, all = TRUE)
 
+# Plot the line chart
+ggplot(extreme_conditions_lisbon_consumption, aes(x = Hour, y = Active.Energy..kWh., color = factor(conditions))) +
+  geom_line(aes(group = conditions)) +
+  labs(title = paste("Average Consumption by Hour and Weather Condition of zip code", zip_code),
+       x = "Hour of the Day",
+       y = "Average Consumption (kWh)",
+       color = "Weather Conditions") +
+  theme_minimal()
--- Trabalho_Pratico/Fase_1/helperFunctions.R
+++ Trabalho_Pratico/Fase_1/helperFunctions.R
@@ -53,3 +53,69 @@ calculate_summary <- function(data) {
   }
   return(summary_df)
 }
+
+VarianceThresholdFeatureSelection = function(dataset, threshold){
+  dataset_original <- dataset
+  
+  # Step 1: Convert all the qualitative features into numerical
+  dataset <- convertQualitativeFeatures(dataset)
+  
+  # Step 2: Normalize the data
+  dataset <- apply(dataset, 2, function(column) column/max(column))
+  
+  # Step 3: Calculate the variance of each feature
+  variances_dataset <- apply(dataset, 2, var)
+  
+  # Sort variances
+  variances_dataset <- sort(variances_dataset, decreasing = TRUE)
+  
+  print(variances_dataset)
+  
+  # Step 3: Calculate the minimum number of features with a cumulative sum of variance of at least threshold value
+  cumulative_sum_variance_above_threshold <- cumsum(variances_dataset) >= threshold
+  
+  reduced_col_number <- which(colnames(dataset) == names(which(cumulative_sum_variance_above_threshold)[1]))[1]
+  
+  return(as.data.frame(dataset_original[,1:reduced_col_number]))
+}
+
+FisherRatioFeatureSelection = function(dataset, class_label, threshold){
+  dataset_original <- dataset
+  
+  dataset <- convertQualitativeFeatures(dataset)
+  
+  # Normalize the data
+  dataset <- apply(dataset, 2, function(column) column/max(column))
+  
+  dataset <- as.data.frame(dataset)
+  
+  dataset_class_means = getClassMeans(dataset, class_label)
+  
+  dataset_class_vars = getClassVars(dataset, class_label)
+  
+  # Compute fishers ratio
+  # Squared subtraction of the means of each class divided by the sum of the variance of each class
+  # All this is done for all attributes
+  # Therefore there will be a Fishers Ratio for each attribute in the dataset
+  dataset_fishers_ratio_numerator = apply(dataset_class_means, MARGIN = 1, function(x) (x[1] - sum(x[-1]))^2)
+  
+  dataset_fishers_ratio_denominator =  apply(dataset_class_vars, MARGIN = 1, sum)
+  
+  fishers_ratio_dataset = dataset_fishers_ratio_numerator / dataset_fishers_ratio_denominator
+  
+  # Replace Inf and NaN values with 0
+  fishers_ratio_dataset[!is.finite(fishers_ratio_dataset)] <- 0
+  
+  # Quick summary of fishers ratio
+  fishers_ratio_dataset <- sort(fishers_ratio_dataset, decreasing = TRUE)
+  threshold_fishers_ratio <- sum(fishers_ratio_dataset) * threshold
+  
+  fisher_ratio_cumulative_sum <- cumsum(fishers_ratio_dataset)
+  
+  last_relevant_feature <- which((fisher_ratio_cumulative_sum >= threshold_fishers_ratio) == TRUE)[1]
+  
+  # The features with a relevance above 0 are stored
+  relevant_features <- names(fishers_ratio_dataset[1:last_relevant_feature])
+  
+  return(as.data.frame(dataset_original[,relevant_features]))
+}
