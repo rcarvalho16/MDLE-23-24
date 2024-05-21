c1829cffb13cb1231c0d5025746da412cdbd5f38
Rafael <rreisdecarvalho@gmail.com>
2024-05-21 22:36:21 +0100

    Adicionei modelos ao energy, adicionei comparação de modelos

--- Trabalho_Pratico/Fase_2/Phase2_energy.R
+++ Trabalho_Pratico/Fase_2/Phase2_energy.R
@@ -27,11 +27,7 @@ fselection_energy <- read.csv("data\\reduced_fs_energy.csv", header = TRUE, stri
 
 freduction_energy[1:5] <- lapply(freduction_energy[1:5], as.factor) %>% lapply(as.numeric)
 
-fselection_energy[2] <- lapply(freduction_energy[2], as.factor) %>% lapply(as.numeric)
-colnames(fselection_energy)[3] <- "labels"
-
 df.freduction_energy <- copy_to(sc, freduction_energy)
-df.fselection_energy <- copy_to(sc, fselection_energy)
 
 ################# Model training ###################
 # Define the split proportions
@@ -39,7 +35,7 @@ train_proportion <- 2 / 3
 test_proportion <- 1 - train_proportion
 
 # Split the dataset into training and testing sets
-splits <- sdf_random_split(df.freduction_energy, training = train_proportion, test = test_proportion, seed = 321)
+splits <- sdf_random_split(df.freduction_energy, training = train_proportion, test = test_proportion, seed = 333)
 
 # Extract the training and testing sets
 df.train <- splits[[1]]
@@ -69,19 +65,42 @@ print(class_distribution_test)
 # Class 2: Residential county
 
 # Train a Random Forest classification model
-rf_model <- ml_random_forest(df.train, labels ~ ., type = "classification", seed = 321)
+rf_model <- ml_random_forest(df.train, labels ~ ., type = "classification", seed = 333)
+lr_model <- ml_logistic_regression(df.train, labels ~ .)
+dt_model <- ml_decision_tree_classifier(df.train, labels ~ ., seed = 333)
 
 # Make predictions on the test dataset
-predictions <- mdle.predict(rf_model, df.test)
+rf_predictions <- mdle.predict(rf_model, df.test)
+lr_predictions <- mdle.predict(lr_model, df.test)
+dt_predictions <- mdle.predict(dt_model, df.test)
+
 
 # Print confusion matrix and evaluate model performance
-mdle.printConfusionMatrix(predictions, "")
+mdle.printConfusionMatrix(rf_predictions, "")
+mdle.printConfusionMatrix(lr_predictions, "")
+mdle.printConfusionMatrix(dt_predictions, "")
 
-predictions <- predictions %>% select('labels', 'prediction')  %>% collect
-roc_value <- roc(df.test_local$labels, predictions$prediction)
+
+
+rf_predictions <- rf_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, rf_predictions$prediction)
 auc_value <- auc(roc_value)
 print("Area under ROC:")
 print(auc_value)
+
+lr_predictions <- lr_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, lr_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
+
+dt_predictions <- dt_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, dt_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
+
+
 
 ################# Applying undersampling ##################
 
@@ -94,7 +113,7 @@ sampling_fraction <- df.pos.train %>% sdf_nrow() / df.neg.train %>% sdf_nrow()
 
 # Given the positive class having way less samples, then we need to undersample the negative class
 undersampled_neg <- df.train %>%
-  filter(labels == 2) %>% sdf_sample(fraction = sampling_fraction, replacement = TRUE, seed = 321)
+  filter(labels == 2) %>% sdf_sample(fraction = sampling_fraction, replacement = TRUE, seed = 333)
 
 # Bind both datasets together
 df.undersample <- df.pos.train %>% sdf_bind_rows(undersampled_neg)
@@ -106,16 +125,35 @@ colnames(class_distribution_undersample) <- column_names
 View(class_distribution_undersample)
 
 # Train the model again
-undersample_rf_model <- ml_random_forest(df.undersample, labels ~ ., type = "classification", seed = 321)
+undersample_rf_model <- ml_random_forest(df.undersample, labels ~ ., type = "classification", seed = 333)
+undersample_lr_model <- ml_logistic_regression(df.undersample, labels ~ .)
+undersample_dt_model <- ml_decision_tree_classifier(df.undersample, labels ~ ., seed = 333)
 
 # Make predictions on the test dataset
-undersample_predictions <- mdle.predict(undersample_rf_model, df.test)
+undersample_rf_predictions <- mdle.predict(undersample_rf_model, df.test)
+undersample_lr_predictions <- mdle.predict(undersample_lr_model, df.test)
+undersample_dt_predictions <- mdle.predict(undersample_dt_model, df.test)
+
 
 # Print confusion matrix and evaluate model performance
-mdle.printConfusionMatrix(undersample_predictions, "")
+mdle.printConfusionMatrix(undersample_rf_predictions, "")
+mdle.printConfusionMatrix(undersample_lr_predictions, "")
+mdle.printConfusionMatrix(undersample_dt_predictions, "")
 
-undersample_predictions <- undersample_predictions %>% select('labels', 'prediction')  %>% collect
-roc_value <- roc(df.test_local$labels, undersample_predictions$prediction)
+undersample_rf_predictions <- undersample_rf_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, undersample_rf_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
+
+undersample_lr_predictions <- undersample_lr_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, undersample_lr_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
+
+undersample_dt_predictions <- undersample_dt_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, undersample_dt_predictions$prediction)
 auc_value <- auc(roc_value)
 print("Area under ROC:")
 print(auc_value)
@@ -124,7 +162,7 @@ print(auc_value)
 
 # Given the positive class having way less samples, then we need to oversample it to match negative class
 oversampled_pos <- df.train %>%
-  filter(labels == 1) %>% sdf_sample(fraction = 1/sampling_fraction, replacement = TRUE, seed = 321)
+  filter(labels == 1) %>% sdf_sample(fraction = 1/sampling_fraction, replacement = TRUE, seed = 333)
 
 # Bind both datasets together
 df.oversample <- df.neg.train %>% sdf_bind_rows(oversampled_pos)
@@ -137,45 +175,37 @@ View(class_distribution_oversample)
 
 
 # Train the model again
-oversample_rf_model <- ml_random_forest(df.oversample, labels ~ ., type = "classification", seed = 321)
+oversample_rf_model <- ml_random_forest(df.oversample, labels ~ ., type = "classification", seed = 333)
+oversample_lr_model <- ml_logistic_regression(df.oversample, labels ~ .)
+oversample_dt_model <- ml_decision_tree_classifier(df.oversample, labels ~ ., seed = 333)
 
 # Make predictions on the test dataset
-oversample_predictions <- mdle.predict(oversample_rf_model, df.test)
+oversample_rf_predictions <- mdle.predict(oversample_rf_model, df.test)
+oversample_lr_predictions <- mdle.predict(oversample_lr_model, df.test)
+oversample_dt_predictions <- mdle.predict(oversample_dt_model, df.test)
 
 # Print confusion matrix and evaluate model performance
-mdle.printConfusionMatrix(oversample_predictions, "")
+mdle.printConfusionMatrix(oversample_rf_predictions, "")
+mdle.printConfusionMatrix(oversample_lr_predictions, "")
+mdle.printConfusionMatrix(oversample_dt_predictions, "")
 
-oversample_predictions <- oversample_predictions %>% select('labels', 'prediction')  %>% collect
-roc_value <- roc(df.test_local$labels, oversample_predictions$prediction)
+oversample_rf_predictions <- oversample_rf_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, oversample_rf_predictions$prediction)
 auc_value <- auc(roc_value)
 print("Area under ROC:")
 print(auc_value)
 
-################# Applying BL-SMOTE #################
+oversample_lr_predictions <- oversample_lr_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, oversample_lr_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
 
-# To apply BLSMOTE, all values must be numeric and therefore we need to convert
-# the discretized ranges to numeric values.
-# NÃO CONSIGO APLICAR NÃO COMPREENDO PORQUÊ (JÁ OLHEI PARA O SOURCE CODE DA FUNÇÃO
-# E COMPREENDO PORQUE NÃO CORRE MAS NÃO COMPREENDO PORQUE É QUE DÁ AQUELES RESULTS)
-
-
-BLSMOTE_train_x <- as.data.frame(collect(df.train %>% select(!labels)))
-BLSMOTE_train_y <- as.data.frame(collect(df.train %>% select(labels)))
-
-BLSMOTE_train_oversampled <- BLSMOTE(X = BLSMOTE_train_x, target = BLSMOTE_train_y, K = 5, C = 5, method = c("type1","type2"))
-
-BLSMOTE_train_oversampled <- as.data.frame(BLSMOTE_train_oversampled$data)
-
-BLSMOTE_train_oversampled <- copy_to(sc, BLSMOTE_train_oversampled)
-
-# f) Repeat points 4.c) and 4.d), and compare the results with the previous models
-oversample_blsmote_rf_model <- ml_random_forest(BLSMOTE_train_oversampled, class ~ ., type = "classification", seed = 321)
-
-# Make predictions on the test dataset
-oversample_blsmote_predictions <- mdle.predict(oversample_blsmote_rf_model, df.test)
-
-# Print confusion matrix and evaluate model performance
-mdle.printConfusionMatrix(oversample_blsmote_predictions, "")
+oversample_dt_predictions <- oversample_dt_predictions %>% select('labels', 'prediction')  %>% collect
+roc_value <- roc(df.test_local$labels, oversample_dt_predictions$prediction)
+auc_value <- auc(roc_value)
+print("Area under ROC:")
+print(auc_value)
 
 ################# Spark cleanup ################
 spark_disconnect(sc)
